10972dc951a30cf12582bd2db9f1839ca291d521
Myranda Hoggatt <hogga102@mail.chapman.edu>
2018-03-13 03:18:22 +0000

    f(x), g(x), s(x) defined

--- array_calculus.py
+++ array_calculus.py
@@ -14,3 +14,34 @@
 #CW06
 ###
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(a,b,n):
+    """Function will return an array of values satisfying the squared 
+    function"""
+    x = np.linspace(a,b,n)
+    return x**2
+
+def s(a,b,n):
+    """Function will return an array of values that satisfy the sine 
+    function"""
+    x= np.linspace(a,b,n)
+
+    def sin(x):
+        return np.sin(x)
+
+    sx= np.array(sin(x))
+    return (x,sx)
+
+def g(a,b,n)
+    """Function will return an array of values satisfying the gaussian 
+    function"""
+    x = np.linspace(a,b,n)
+
+    def gauss(x):
+        return (1/np.sqrt(2*np.pi))*np.exp(-x**2/2)
+
+    g = np.array(gauss(x))
+    return (x, g)
