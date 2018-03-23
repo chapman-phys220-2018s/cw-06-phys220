482e876e8916d18dd1e457b6f5e4ffff242be2e8
Myranda Hoggatt <hogga102@mail.chapman.edu>
2018-03-23 01:57:56 +0000

    derivative test created for sin

--- test_array_calculus.py
+++ test_array_calculus.py
@@ -1,5 +1,5 @@
-
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 ###
 #Daniel Chang
@@ -14,3 +14,23 @@
 #PHYS220 Spring 2018
 #CW06
 ###
+
+import numpy as np
+import nose
+import array_calculus as arcalc
+
+def test_derivative():
+    """Test_derivative() tests the derivative matrix for the chosen values of
+    x= -1, 0, and 1."""
+    desired = np.array([(np.sin(0)-np.sin(-1)),(np.sin(1)-np.sin(-1)/2),(np.sin(1)-np.sin(0))])
+    def sin():
+        x = np.linspace(-1,1,3)
+        sin = np.sin(x)
+        return sin
+    obtained = np.array(arcalc.derivative(-1,1,3), sin())
+    print("Desired: ",desired)
+    print("Obtained: ",obtained)
+    np.testing.assert_almost_equal(obtained, desired)
+
+def test_second_derivative():
+
